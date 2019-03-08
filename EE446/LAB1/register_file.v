aaca8484a849bfe062b2157494df8e5a2dc4e61a
Caner TOL <36196040+canertol@users.noreply.github.com>
2019-03-08 16:16:35 +0300

    Update register_file.v

--- EE446/LAB1/register_file.v
+++ EE446/LAB1/register_file.v
@@ -1,3 +1,20 @@
-module register_file  #(parameter, W = 3)(data_in, data_out1, data_out2)
-
+module register_file  #(parameter, W = 3)(
+       clk, 
+       WD3, WE3, rst
+       A1, A2, A3, 
+       RD1, RD2);
+  
+       // inputs
+       input clk;
+       input [W:0] data_in;
+       input [1:0] A1, A2, A3;
+       input WE3, rst;
+       // outputs
+       output reg [W:0] RD1, RD2;
+  
+  
+  
+  
+  
+  
 endmodule

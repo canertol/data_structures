1494607f33a253b9d13d4bb98b6f5d09a61ffbcc
Caner TOL <36196040+canertol@users.noreply.github.com>
2019-03-13 08:51:16 +0300

    Update Controller_Unit.v

--- EE446/LAB2/Controller_Unit.v
+++ EE446/LAB2/Controller_Unit.v
@@ -1,10 +1,23 @@
-module controller(
-                CLK, RST,
-                AccRight, AccParallel, // Acc register control
-                OP, ASrc, BSrc,  // ALU Controllers
-                Stat, NFlag,    // Status bits
-                INST // Instruction
+module controller(CLK,
+            AccRight, AccParallel, // Acc register control
+            ALUCtrl, ASrc, BSrc,  // ALU Controllers
+            Stat, NFlag,    // Status bits
+            LOAD,
+            COMP, 
+            OP,  //ALP Operation
+            CLR, // reset registers
+            ERR // Arithmetic overflow
+            
               );
+     input CLK;
+     input LOAD, COMP, CLR;
+     input [2:0] OP;
+     input [1:0] Stat;
+     input Nflag;
+     output reg ERR;
+     output wire AccRight, AccParallel, ALUCtrl, ASrc, BSrc;
+     
+     
 
 
 
